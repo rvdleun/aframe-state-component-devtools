7592aff9e49254df910ec5e817ede341385f5b83
Ruben van der Leun <ruben@leunix.nl>
2021-06-28 12:35:22 +0200

    Initial init

---
+++ README.md
@@ -0,0 +1,9 @@
+# aframe-state-component-devtools
+
+A simple AFrame system that adds support for [aframe-state-component](https://github.com/supermedium/superframe/tree/master/components/state) and [Redux DevTools](https://github.com/reduxjs/redux-devtools). 
+
+You only need to add or import the `index.js` file to enable it.
+
+```javascript
+require('aframe-state-component-devtools');
+```
---
+++ index.js
@@ -0,0 +1,28 @@
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+AFRAME.registerSystem('state-devtools', {
+    init: function() {
+        this.onRenderStart = this.onRenderStart.bind(this);
+        this.updateState = this.updateState.bind(this);
+
+        const { el } = this;
+        if (el.renderStarted) {
+            this.onRenderStart();
+        } else {
+            el.addEventListener("renderstart", this.onRenderStart);
+        }
+    },
+
+    onRenderStart: function() {
+        const { el } = this;
+
+        el.removeEventListener("renderstart", this.onRenderStart);
+        el.addEventListener("stateupdate", e => this.updateState(e.detail.action));
+
+        this.updateState('init');
+    },
+
+    updateState: function(event) {
+        reduxDevTools.send(event, this.el.systems.state.state);
+    }
+});
